14c9870f20f4f757875f5ccc4e2a5fa51fe6a3a3
Mans-N <mans@neikter.se>
2025-01-09 10:56:53 +0100

    Added new graph

--- Containers/cljdetector/MonitorTool/monitor.ts
+++ Containers/cljdetector/MonitorTool/monitor.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import express from "express";
 import HTMLPage from "./page";
 
+const PROCESSING_CHUNKS_DONE = "Identifying Clone Candidates...";
+const EXPANDING_CANDIDATES_DONE = "Summary";
+const IDENTIFY_CANDIDATES_DONE = /Found\d+candidates/;
 const dbHost = process.env.DBHOST || "localhost";
 const dbPort = process.env.DBPORT || "27017";
 const dbName = process.env.DBNAME || "cloneDetector";
@@ -21,12 +24,35 @@ type ProcessedData = {
   chunks: number;
 };
 
-const processedData: ProcessedData[] = [];
+const processedChunks: ProcessedData[] = [];
+const identifiedCandidates: {
+  timestamp: Date;
+  candidates: number;
+}[] = [];
+const identifiedClones: {
+  timestamp: Date;
+  clones: number;
+}[] = [];
+
 let filesCount = 0;
 let chunksCount = 0;
 let candidatesCount = 0;
 let clonesCount = 0;
 let startTime = new Date();
+let dbClones: {
+  _id: any;
+  numberOfInstances: number;
+  instances: {
+    fileName: string;
+    startLine: number;
+    endLine: number;
+  }[];
+}[] = [];
+let statusUpdates: {
+  _id: any;
+  timestamp: string;
+  message: string;
+}[] = [];
 
 const fetchStatistics = async () => {
   try {
@@ -39,12 +65,15 @@ const fetchStatistics = async () => {
     filesCount = await db.collection("files").countDocuments();
     chunksCount = await db.collection("chunks").countDocuments();
     candidatesCount = await db.collection("candidates").countDocuments();
-    clonesCount = await db.collection("clones").countDocuments();
-    const statusUpdates = await db
+
+    dbClones = (await db.collection("clones").find({}).toArray()) as any;
+    clonesCount = dbClones.length;
+
+    statusUpdates = (await db
       .collection("statusUpdates")
       .find()
       .sort({ timestamp: -1 })
-      .toArray();
+      .toArray()) as any;
 
     console.log("Current Statistics: \n");
     console.log(`Files: ${filesCount} \n`);
@@ -56,10 +85,40 @@ const fetchStatistics = async () => {
       console.log(`${update.timestamp}: ${update.message} \n`);
     });
 
-    processedData.push({
-      timestamp: new Date(),
-      chunks: chunksCount,
-    });
+    const processingChunksDone = Boolean(
+      statusUpdates.find(({ message }) =>
+        (message as string).startsWith(PROCESSING_CHUNKS_DONE)
+      )
+    );
+
+    const identifyCandidatesDone = Boolean(
+      statusUpdates.find(({ message }) =>
+        IDENTIFY_CANDIDATES_DONE.test(message)
+      )
+    );
+
+    const expandingCandidatesDone = Boolean(
+      statusUpdates.find(({ message }) => message === EXPANDING_CANDIDATES_DONE)
+    );
+
+    const now = new Date();
+
+    if (chunksCount && !processingChunksDone) {
+      processedChunks.push({
+        timestamp: new Date(now),
+        chunks: chunksCount,
+      });
+    }
+    if (identifyCandidatesDone && !expandingCandidatesDone) {
+      identifiedCandidates.push({
+        timestamp: new Date(now),
+        candidates: candidatesCount,
+      });
+      identifiedClones.push({
+        timestamp: new Date(now),
+        clones: clonesCount,
+      });
+    }
   } catch (err) {
     console.error("Error fetching statistics:", err);
   }
@@ -78,20 +137,13 @@ app.get("/", async (req, res, next) => {
     res.send(page.get());
     return;
   }
-
-  page.addParagraphs(
-    `Files: ${filesCount}`,
-    `Chunks: ${chunksCount}`,
-    `Candidates: ${candidatesCount}`,
-    `Clones: ${clonesCount}`
-  );
   const chunksContraTime = {
     graphId: "processedGraph",
     label: "Data visualization",
     xLabel: "Time (s)",
     yLabel: "Chunks",
     xScale: { type: "linear" },
-    data: processedData.map(({ timestamp, chunks }) => ({
+    data: processedChunks.map(({ timestamp, chunks }) => ({
       x: (timestamp.getTime() - startTime.getTime()) / 1000,
       y: chunks,
     })),
@@ -102,8 +154,8 @@ app.get("/", async (req, res, next) => {
     xLabel: "Time (s)",
     yLabel: "Chunks",
     xScale: { type: "linear" },
-    data: processedData.map(({ timestamp, chunks }, i) => {
-      const prevChunks = processedData[i - 1]?.chunks || 0;
+    data: processedChunks.map(({ timestamp, chunks }, i) => {
+      const prevChunks = processedChunks[i - 1]?.chunks || 0;
       return {
         x: (timestamp.getTime() - startTime.getTime()) / 1000,
         y: chunks - prevChunks,
@@ -116,11 +168,11 @@ app.get("/", async (req, res, next) => {
     xLabel: "Total chunks",
     yLabel: "Processing time (ms)",
     xScale: { type: "linear" },
-    data: processedData.map(({ timestamp, chunks }, i) => {
+    data: processedChunks.map(({ timestamp, chunks }, i) => {
       // Not 100% accurate due to starting order of containers
       const prevTime =
-        processedData[i - 1]?.timestamp.getTime() || startTime.getTime();
-      const newChunks = chunks - (processedData[i - 1]?.chunks || 0);
+        processedChunks[i - 1]?.timestamp.getTime() || startTime.getTime();
+      const newChunks = chunks - (processedChunks[i - 1]?.chunks || 0);
       const interval = timestamp.getTime() - prevTime;
       return {
         y: interval / (newChunks || 1), // chunks per interval
@@ -128,6 +180,44 @@ app.get("/", async (req, res, next) => {
       };
     }),
   };
+
+  const expandedClones = {
+    graphId: "expandedClonesGraph",
+    label: "New clones compared to candidates",
+    xLabel: "Candidates",
+    yLabel: "New clones created for interval",
+    xScale: { type: "linear" },
+    data: identifiedClones.map(({ timestamp, clones }, i) => {
+      // Not 100% accurate due to starting order of containers
+      // nya clones / nya candidates
+      const newClones = clones - (identifiedClones[i - 1]?.clones || 0);
+      return {
+        y: newClones, // chunks per interval
+        x: identifiedCandidates[i] || 0,
+      };
+    }),
+  };
+
+  const candidates =
+    statusUpdates
+      .find(({ message }) => IDENTIFY_CANDIDATES_DONE.test(message))
+      ?.message.match(/\d+/)?.[0] || "0";
+
+  const avgCloneSize =
+    dbClones.reduce((avg, { instances }) => {
+      const { startLine, endLine } = instances[0];
+      return avg + (endLine - startLine);
+    }, 0) / (clonesCount || 1);
+
+  page.addParagraphs(
+    `Files: ${filesCount}`,
+    `Chunks: ${chunksCount}`,
+    `Candidates: ${candidatesCount}`,
+    `Clones: ${clonesCount}`,
+    `Average number of chunks per file: ${chunksCount / (filesCount || 1)}`,
+    `Average clone size: ${avgCloneSize}`
+  );
+
   page.addTitle("Chunks growth over time");
   page.addGraph(chunksContraTime);
   page.addTitle("Chunks generated per interval");
@@ -138,6 +228,9 @@ app.get("/", async (req, res, next) => {
   );
   page.addParagraphs("First value is inaccurate due to start up");
   page.addGraph(processingTime);
+  page.addTitle("Expanded candidates vs remaining candidates");
+  page.addParagraphs(`Total number of candidates: ${candidates}`);
+  page.addGraph(expandedClones);
   res.send(page.get());
 });
 
